9baf47fe7a671b0030c43c8fcbe38cd0e21f6ba6
galvezn <galvez2902@icloud.com>
2020-10-11 11:04:56 -0500

    completed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,8 @@
-require_relative "../lib/catch_phrase.rb"
-require_relative "../lib/rescue_princess_peach.rb"
-require 'pry'
+require "spec_helper"
 
-
-RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+describe "#rescue_princess_peach" do
+  it "outputs a message and sets a variable, status, that is not available outside of this method" do
+    expect{rescue_princess_peach}.to output("Hooray! Mario has rescued Princess Peach.\n").to_stdout
+    expect{puts status}.to raise_error(NameError)
   end
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-
 end
